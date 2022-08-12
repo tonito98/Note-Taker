c134683e9657c97d19c6bf889208e1484a21e43e
tonito98 <86890715+tonito98@users.noreply.github.com>
2022-08-12 22:36:50 +0200

    generate a note and once it is saved it is displayed on the left-hand column

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -40,6 +40,16 @@ const saveNote = (note) =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(note),
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    alert('Error: ' + response.statusText);
+  })
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding a note!');
   });
 
 const deleteNote = (id) =>
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const { notes } = require('./db/db.json');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended:true }));
+// parse incoming JSON data
+app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 // To serve the html file and the js and css files connected to it.
 app.use(express.static('public'));
 
+function createNewNote(body, notesArray) {
+    const note =body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+       JSON.stringify({ notes: notesArray }, null, 2) 
+    );
+    return note;
+}
 //Redirects '/' route to index
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -19,8 +33,24 @@ app.get('/notes', (req,res) => {
 });
 
 // Wildcard route, if a client makes a request to a nonexistent route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+
+// API routes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    //set id based on what  the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // add nones to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+
+    res.json(note);
 });
 
 app.listen(PORT, () => {
